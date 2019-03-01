e9bf627032c994a7738b84e1532eb31c74a17b86
avikju <avikju@gmail.com>
2019-03-01 10:05:39 +0530

    Updated jar Spring security

---
+++ springbootdemo/src/main/java/com/avik/springbootdemo/security/SecurityConfiguration.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.avik.springbootdemo.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Avik
+ *
+ */
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+}
